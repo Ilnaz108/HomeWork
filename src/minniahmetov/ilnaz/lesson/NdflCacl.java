9161e86377190d90f2d94cd69ca3525c2dc72614
Ilnaz108 <66380719+Ilnaz108@users.noreply.github.com>
2020-06-13 11:00:19 +0300

    Update NdflCacl.java

--- src/minniahmetov/ilnaz/lesson/NdflCacl.java
+++ src/minniahmetov/ilnaz/lesson/NdflCacl.java
@@ -3,7 +3,6 @@ package minniahmetov.ilnaz.lesson;
 public class NdflCacl  {
      public String calc(int value) {
          String result = null;
-         // TODO тут добавляешь свой код который вычисляет НДФЛ
          return result;
      }
 }
