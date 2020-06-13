9aaf6624d743be323cb337a95fdaabc0c2a28e40
Ilnaz108 <66380719+Ilnaz108@users.noreply.github.com>
2020-06-13 11:01:01 +0300

    Update SecToHoursCalc.java

--- src/minniahmetov/ilnaz/lesson/SecToHoursCalc.java
+++ src/minniahmetov/ilnaz/lesson/SecToHoursCalc.java
@@ -3,7 +3,6 @@ package minniahmetov.ilnaz.lesson;
 public class SecToHoursCalc {
     public String calc(int value) {
         String result = null;
-        // TODO тут добавляешь свой код который вычисляет часы
         return result;
     }
 }
