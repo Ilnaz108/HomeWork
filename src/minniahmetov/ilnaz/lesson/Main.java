497db037c59258381d7f37c616f3a16e7aa72af6
Ilnaz108 <66380719+Ilnaz108@users.noreply.github.com>
2020-06-13 11:09:40 +0300

    Update Main.java

--- src/minniahmetov/ilnaz/lesson/Main.java
+++ src/minniahmetov/ilnaz/lesson/Main.java
@@ -1,5 +1,3 @@
-package minniahmetov.ilnaz.lesson;
-
 import java.util.Scanner;
 
 public class Main {
@@ -9,27 +7,27 @@ public class Main {
         NdflCacl ndflCacl = new NdflCacl();
         SecToHoursCalc secToHoursCalc = new SecToHoursCalc();
         System.out.println("Выберите программу:\n" +
-                "1 - НДФЛ\n" +
-                "2 - Бензин\n" +
+                "1 - Бензин\n" +
+                "2 - НДФЛ\n" +
                 "3 - Секунда в часы\n");
         Scanner scanner = new Scanner(System.in);
         int type = scanner.nextInt();
         switch (type) {
             case 1: {
                 System.out.println("Введите количество литров");
-                int litrs = scanner.nextInt();
+                double litrs = scanner.nextInt();
                 gasolineCalc.calc(litrs);
             }
             break;
             case 2: {
                 System.out.println("Введите зарплату");
-                int salary = scanner.nextInt();
+                double salary = scanner.nextDouble();
                 ndflCacl.calc(salary);
             }
             break;
             case 3: {
                 System.out.println("Введите количество секунд");
-                int sec = scanner.nextInt();
+                double sec = scanner.nextDouble();
                 secToHoursCalc.calc(sec);
             }
             break;
